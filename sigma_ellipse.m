cefc0d731ae3158c29db18509c8d5b35cb4dc742
Parth Viradiya <36721553+parthp08@users.noreply.github.com>
2019-12-02 23:01:47 -0500

    Update sigma_ellipse.m

--- sigma_ellipse.m
+++ sigma_ellipse.m
@@ -1,14 +1,14 @@
 %%% calculation for 1-sigma error
 %%% have to compte the DOP matrix in ENU frame
-%%% 1-sigma ellipse means 68.2689492% of the time values will be 
-%%% within the boundary of ellipse [5].
+%%% Note: have to run main.m file before running this file
 %
 %%% References
-% [1] https://www.mathworks.com/matlabcentral/answers/86615-how-to-plot-an-ellipse#
-% [2] 'GNSS Error Sources',Malek Karaim, Mohamed Elsheikh and Aboelmagd Noureldin 
-% [3] https://www.xarg.org/2018/04/how-to-plot-a-covariance-error-ellipse/
-% [4] https://www.visiondummy.com/2014/04/draw-error-ellipse-representing-covariance-matrix/
-% [5] https://en.wikipedia.org/wiki/Standard_deviation
+% -------------
+%[1] 'Global Positioning System: Theory and Applications'; edited by 
+%     Parkinson, Spilker, Axelrad, Enge; AIAA; 1996
+%[2] 'Aerospace Navigation Systems'; edited by A.V.Nebylv, J.Watson
+%[3] https://www.visiondummy.com/2014/04/draw-error-ellipse-representing-covariance-matrix/
+%[4] https://stattrek.com/online-calculator/chi-square.aspx
 
 % use Pu and P_sat_arr from main files and convert them to ENU Frame
 Pu_enu = ECEF2ENU(Pu);
@@ -33,16 +33,31 @@ GDOP_enu = sqrt(DOP_enu(1,1)+DOP_enu(2,2)+DOP_enu(3,3)+DOP_enu(4,4)); % geometri
 PDOP_enu = sqrt(DOP_enu(1,1)+DOP_enu(2,2)+DOP_enu(3,3)); % Position DOP
 HDOP_enu = sqrt(DOP_enu(1,1)+DOP_enu(2,2)); % Horizontal DOP
 
-%%%%%% 1-sigma ellipse  % Ref [1] and [3]
-% 1-sigma ellipse in East and North Direction
-x_e = DOP_enu(1,1); % semimajor axis of an ellipse
-x_n = DOP_enu(2,2); % semiminor axis of an ellipse
+UERE = 5.3; % page 481 Ref[1] % 1-sigma user equivalent error
+
+% for east and north plane error
+ENDOP = DOP_enu(1:2,1:2);
+
+% covarience matrix
+K = (UERE^2)*ENDOP; % it is correlated  %%%%%%%% put UERE^2
+sigma_xe = sqrt(K(1,1));
+sigma_xn = sqrt(K(2,2));
+
+S = 2.28; % scale of the ellipse for 1-sigma (68.2% confidence)
+[V,E] = eig(ENDOP);
+a = sigma_xe*sqrt(S); % semi-major axis % in north direction
+b = sigma_xn*sqrt(S); % semi-minor axis % in east direction
+theta = atan(V(2,2)/V(2,1)); % orientation of the ellipse
+
 xe0=0; % x0,y0 ellipse centre coordinates
 xn0=0;
 t=-pi:0.01:pi;
-XE=xe0+x_e*cos(t);
-XN=xn0+x_n*sin(t);
-plot(XE,XN);
+XE=xe0+a*cos(t);
+XN=xn0+b*sin(t);
+% rotate ellipse to theta angle
+XE_rot = XE*cos(theta) + -XN*sin(theta);
+XN_rot = XE*(sin(theta)) + XN*cos(theta);
+plot(XE_rot,XN_rot);
 title("1-sigma ellipse");
 xlabel("X east");
 ylabel("X north");
